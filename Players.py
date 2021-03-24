34eafdf5e076de45a63237f74f732f1786338149
marceljordan0 <75436070+marceljordan0@users.noreply.github.com>
2021-03-24 16:01:56 +0100

    player

--- Players.py
+++ Players.py
@@ -5,29 +5,18 @@ players = {}
 class players():"""
 
 
-class Meta(type):
-    def __new__(mcs, name, bases, dct):
-        mcs.x = super().__new__(mcs, name, bases, dct)
-        mcs.x.attr = 2000
-        setattr(mcs.x, mcs.test.__name__, mcs.test)
-        setattr(mcs.x, mcs.draw_pieces.__name__, mcs.draw_pieces)
-        return mcs.x
+class Player:
+    def __init__(self, players, coordinates):
+        self._players = players
+        self.coordinates = coordinates
 
-    def test(cls):
-        print(1000)
+    @property
+    def players(self):
+        return self._players
 
-    def draw_pieces(cls):
-        pass
+    @players.setter
+    def players(self, name, value=None):
+        if value is None:
+            return
+        self._players[name]=value
 
-
-class foo(metaclass=Meta):
-
-    def __init__(self):
-        x = 23
-
-
-class foo2(metaclass=Meta):
-    pass
-
-
-print(foo().test())
