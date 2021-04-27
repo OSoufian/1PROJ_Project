fd7f9233c403d88e7a70c90f71418a43f6ca5920
loki7even <lounes.behloul@supinfo.com>
2021-04-27 14:43:27 +0200

    Revert "player"
    This reverts commit 34eafdf5e076de45a63237f74f732f1786338149.

--- Players.py
+++ Players.py
@@ -5,18 +5,29 @@ players = {}
 class players():"""
 
 
-class Player:
-    def __init__(self, players, coordinates):
-        self._players = players
-        self.coordinates = coordinates
+class Meta(type):
+    def __new__(mcs, name, bases, dct):
+        mcs.x = super().__new__(mcs, name, bases, dct)
+        mcs.x.attr = 2000
+        setattr(mcs.x, mcs.test.__name__, mcs.test)
+        setattr(mcs.x, mcs.draw_pieces.__name__, mcs.draw_pieces)
+        return mcs.x
 
-    @property
-    def players(self):
-        return self._players
+    def test(cls):
+        print(1000)
 
-    @players.setter
-    def players(self, name, value=None):
-        if value is None:
-            return
-        self._players[name]=value
+    def draw_pieces(cls):
+        pass
 
+
+class foo(metaclass=Meta):
+
+    def __init__(self):
+        x = 23
+
+
+class foo2(metaclass=Meta):
+    pass
+
+
+print(foo().test())
