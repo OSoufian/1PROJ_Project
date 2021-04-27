d3810486be6374586e51a4fa41fc3c0cde653b2e
cyprien <cyprientaib1@gmail.com>
2021-04-27 15:50:27 +0200

    Collision Made

--- main.py
+++ main.py
@@ -15,18 +15,22 @@ pieces = {"p1": [], "p2": []}
 
 while running:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            print(pygame.mouse.get_pos())
-
     board = CreateBoard([e // 2 for e in size], 250)
     board.draw_regular_polygon(screen, (255, 255, 255), outline=10)
     coordinates = board.draw_ceil(screen, (0, 0, 0), 2)
 
-    for i in coordinates[:-1]:
-        pygame.draw.circle(screen, "black", i, coordinates[-1], 2)
+    sprites = [pygame.draw.circle(screen, "black", i, coordinates[-1], 2) for i in coordinates[:-1]]
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            clicked_sprites = [s for s in sprites if s.collidepoint(pos)]
+
+            if len(clicked_sprites) >= 1:
+                print(clicked_sprites[0].center)
+
 
     play = ""
 
