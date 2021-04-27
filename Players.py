12beaac2d7328516cc309dc8223a912b6aa178ab
cyprien <cyprientaib1@gmail.com>
2021-04-27 14:23:24 +0200

    Player is done

--- Players.py
+++ Players.py
@@ -1,22 +1,66 @@
-"""import pygame
+class Player(object):
+    __player = {}
 
-players = {}
+    def __new__(cls, **kwargs):
 
-class players():"""
+        return super(Player, cls).__new__()
 
+    def __init__(self, **kwargs):
+        players_data = ["circles", "colorn" "points", "nom"]
+        for k in kwargs.keys():
+            if k not in players_data:
+                del kwargs[k]
 
-class Player:
-    def __init__(self, players, coordinates):
-        self._players = players
-        self.coordinates = coordinates
+        self.__player = kwargs
+
+        self .get_id()
+
+    @classmethod
+    def get_id(cls):
+        cls.__player["id"] = len(cls.__player.keys())-1
+        
+    @property
+    def circles(self) -> list:
+
+        return self.__player["circles"] if "circles" in self.__player else None
+
+    @circles.setter
+    def circles(self, circles, remove=False):
+        if "circles" not in self.__player:
+            self.__player["circles"] = []
+            remove = False
+        if remove:
+            self.__player["circles"].remove(circles)
+        self.__player["circles"].append(circles)
 
     @property
-    def players(self):
-        return self._players
+    def color(self) -> str:
+        return self.__player["color"] if "color" in self.__player else None
 
-    @players.setter
-    def players(self, name, value=None):
-        if value is None:
-            return
-        self._players[name]=value
+    @color.setter
+    def color(self, color):
+        self.__player["color"] = color
+
+    @property
+    def points(self) -> int:
+        return self.__player["points"] if "points" in self.__player else None
+
+    @points.setter
+    def points(self, points, remove=False):
+        if "points" not in self.__player:
+            self.__player["points"] = points
+
+        elif remove:
+            self.__player["points"] += points
+        else:
+            self.__player["points"] -= points
+
+    @property
+    def getName(self) -> str:
+        return self.__player["name"] if "name" in self.__player else None
+
+    @getName.setter
+    def getName(self, name):
+        self.__player["name"] = name
+
 
