7b4036f3b38a2710373cca48dd105e7c763ca4a4
marceljordan0 <75436070+marceljordan0@users.noreply.github.com>
2021-06-06 23:11:17 +0200

    changement du nom des variables

--- interface.py
+++ interface.py
@@ -13,11 +13,11 @@ class Jeu:
         self.image_modif = pygame.transform.scale(self.image, (150, 150))
 
         # l'image bouton
-        self.button = pygame.image.load('./Menu/play.png')
-        self.button = pygame.transform.scale(self.button, (200, 100))
-        self.button_play = self.button.get_rect()
-        self.button_play.x = 320
-        self.button_play.y = 450
+        self.bouton = pygame.image.load('./Menu/play.png')
+        self.bouton = pygame.transform.scale(self.bouton, (200, 100))
+        self.bouton_coord = self.bouton.get_rect()
+        self.bouton_coord.x = 320
+        self.bouton_coord.y = 450
 
         self.parametre = pygame.image.load('./Menu/parametre.png')
         self.parametre = pygame.transform.scale(self.parametre, (80, 80))
@@ -25,31 +25,31 @@ class Jeu:
         # l'image suivant (regle)
         self.suivant = pygame.image.load("./Menu/suivant.png")
         self.suivant = pygame.transform.scale(self.suivant, (50, 50))
-        self.suivant_play = self.suivant.get_rect()
-        self.suivant_play.x = 700
-        self.suivant_play.y = 700
+        self.suivant_coord = self.suivant.get_rect()
+        self.suivant_coord.x = 700
+        self.suivant_coord.y = 700
 
         # l' image regle
         self.regle = pygame.image.load("./Menu/regle.png")
         
 
         # image bouton jouer (configuration)
-        self.rule = pygame.image.load("Menu/rule.png")
-        self.rule = pygame.transform.scale(self.rule, (50, 50))
+        self.img_livre = pygame.image.load("Menu/rule.png")
+        self.img_livre = pygame.transform.scale(self.img_livre, (50, 50))
         self.jouer = pygame.image.load('./Menu/play.png')
         self.jouer = pygame.transform.scale(self.jouer, (200, 100))
-        self.jouer_play = self.jouer.get_rect()
-        self.jouer_play.x = 320
-        self.jouer_play.y = 680
+        self.jouer_coord = self.jouer.get_rect()
+        self.jouer_coord.x = 320
+        self.jouer_coord.y = 680
 
         # image de fond ecran
         self.fond = pygame.image.load("./Menu/fond.jpg")
         self.fond = pygame.transform.scale(self.fond, self.ecran.get_size())
 
-        self.list_configuration = ["Standard", "Domanation", "Face à Face", "Fujiyama", "Infiltration",
+        self.liste_configuration = ["Standard", "Domanation", "Face à Face", "Fujiyama", "Infiltration",
                                    "Marguerite allemande", "Marguerite belge", "Marguerite Hollandaise",
                                    "Marguerite suisse", "Pyramide", "Snake variante", "Personnalisé"]
-        self.list_player = ["1", "2", "3", "4"]
+        self.liste_player = ["1", "2", "3", "4"]
 
     def master(self):
 
@@ -58,12 +58,12 @@ class Jeu:
             for evenement in pygame.event.get():
                 if evenement.type == pygame.QUIT:
                     sys.exit()
-                if evenement.type == pygame.MOUSEBUTTONDOWN and self.button_play.collidepoint(evenement.pos):
+                if evenement.type == pygame.MOUSEBUTTONDOWN and self.bouton_coord.collidepoint(evenement.pos):
                     self.accueil = False
                     self.ecran_regle = True
                 self.message("grande", "WELCOME TO ABALONE ", (30, 220, 100, 50), (255, 255, 255))
                 self.ecran.blit(self.image_modif, (350, 280, 100, 50))
-                self.ecran.blit(self.button, self.button_play)
+                self.ecran.blit(self.bouton, self.bouton_coord)
                 pygame.display.flip()
 
         while self.ecran_regle:
@@ -71,12 +71,12 @@ class Jeu:
             for evenement in pygame.event.get():
                 if evenement.type == pygame.QUIT:
                     sys.exit()
-                if evenement.type == pygame.MOUSEBUTTONDOWN and self.suivant_play.collidepoint(evenement.pos):
+                if evenement.type == pygame.MOUSEBUTTONDOWN and self.suivant_coord.collidepoint(evenement.pos):
                     self.ecran_regle = False
                     self.ecran_debut = True
                 self.message("moyenne", "Règles", (300, 50, 100, 50), (255, 255, 255))
-                self.ecran.blit(self.suivant, self.suivant_play)
-                self.ecran.blit(self.rule, (460, 48, 100, 50))
+                self.ecran.blit(self.suivant, self.suivant_coord)
+                self.ecran.blit(self.img_livre, (460, 48, 100, 50))
                 self.ecran.blit(self.regle, (15, 120, 100, 50))
                 pygame.display.flip()
 
@@ -89,16 +89,16 @@ class Jeu:
                 self.message("moyenne", "Configuration", (160, 50, 100, 50), (255, 255, 255))
                 self.message("petite", "Number of players", (350, 140, 100, 50), "black")
                 self.message("petite", "configuration table", (350, 300, 100, 50), "black")
-                self.ecran.blit(self.jouer, self.jouer_play)
+                self.ecran.blit(self.jouer, self.jouer_coord)
                 self.ecran.blit(self.parametre, (550, 30, 100, 50))
 
                 # checkbox for player
                 k = 0
                 for i in range(0, 300, 60):
                     pygame.draw.rect(self.ecran, "WHITE", (340 + i, 200, 15, 15))
-                    self.message("petite", self.list_player[k], (341 + i, 220, 100, 50), "black")
+                    self.message("petite", self.liste_player[k], (341 + i, 220, 100, 50), "black")
                     k += 1
-                    if k == len(self.list_player):
+                    if k == len(self.liste_player):
                         break
 
                 # checkbox for table
@@ -106,11 +106,11 @@ class Jeu:
                 for i in range(0, 600, 300):
                     for j in range(0, 300, 50):
                         pygame.draw.rect(self.ecran, "WHITE", (200 + i, 363 + j, 15, 15))
-                        self.message("petite", self.list_configuration[k], (220 + i, 360 + j, 100, 50), "black")
+                        self.message("petite", self.liste_configuration[k], (220 + i, 360 + j, 100, 50), "black")
                         k += 1
-                        if k == len(self.list_configuration):
+                        if k == len(self.liste_configuration):
                             break
-                if evenement.type == pygame.MOUSEBUTTONDOWN and self.jouer_play.collidepoint(evenement.pos):
+                if evenement.type == pygame.MOUSEBUTTONDOWN and self.jouer_coord.collidepoint(evenement.pos):
                     self.ecran_debut = False
                 pygame.display.flip()
 
