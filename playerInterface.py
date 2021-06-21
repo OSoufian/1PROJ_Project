5307a17d0ac4a0c1a897399045b5abd83b51b76e
akito-sama <ahmedquadimi22@gmail.com>
2021-06-21 12:46:42 +0100

    add possible entry

--- playerInterface.py
+++ playerInterface.py
@@ -3,48 +3,41 @@ import pygame as pg
 
 class PlayerInterface():
 
-    def run(self, screen, nb_joueur):
-        players = []
-        for player in range(nb_joueur):
-            name= color = "" 
+    def run(self, screen, players):
+        for player in players:
+            name = ""
             active_name = active_color = False 
             validate = True
-            font = pg.font.Font(None, 32)
-            input_name = pg.Rect(100, 100, 140, 32) 
+            font = pg.font.SysFont("arial", 32)
+            input_name = pg.Rect(100, 100, 140, 40)
             input_color = pg.Rect(100, 200, 140, 32)
-            validate_input = pg.Rect(1000, 300, 140, 32)
+            validate_input = pg.Rect(100, 300, 140, 32)
 
             while validate:
                 for event in pg.event.get():
 
                     if event.type == pg.MOUSEBUTTONDOWN:
                         active_name = input_name.collidepoint(event.pos)
-                        active_color = input_color.collidepoint(event.pos)
                         validate = validate_input.collidepoint(event.pos)
-            
+
                     if event.type == pg.KEYDOWN:
             
                         if event.key == pg.K_BACKSPACE:
-                            
-                            if  active_name:
-                                name = name[:-1]
-                            else:
-                                color = color[:-1]
-                            
-                        elif  active_name:
-                            name += event.unicode
-                                
+                            name = name[:-1]
                         else:                                
-                            color += event.unicode
+                            name += event.unicode
 
-                txt_name = font.render(name, True, color)
+                    if event.type == pg.QUIT:
+                        pg.quit()
 
-                txt_color = font.render(color, True, color)
+                pg.draw.rect(screen, "#555555", input_name, border_radius=10)
+                txt_name = font.render(name, False, "black")
+                screen.blit(txt_name, input_name)
+                print(name)
+                pg.display.flip()
 
-                Nwidth = max(30, txt_name.get_width()+10)
+
+                Nwidth = txt_name.get_width()
                 input_name.w = Nwidth
-
-                Cwidth = max(30, txt_color.get_width()+10)
-                input_color.w = Cwidth
-                
-                pg.screen.blit()
+            
+            player.name = name
