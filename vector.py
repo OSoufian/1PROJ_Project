d788eddc105e03553f872bcaa15697688ff2372d
loki7even <lounes.behloul@supinfo.com>
2021-06-23 00:35:08 +0200

    un peu de clean

--- vector.py
+++ vector.py
@@ -6,15 +6,6 @@ class Vector2:
     
     def convert(self, x, y):
         return Vector2((x+self.x, y+self.y), 1)
-
-    def __neg__(self):
-        return Vector2((self.x * - 1, self.y * -1), self.lenght)
-    
-    def __eq__(self, vecteur) -> bool:
-        return self.lenght == vecteur.lenght
-    
-    def __lt__(self, vecteur) -> bool:
-        return self.lenght < vecteur.lenght
     
     def __str__(self):
         return f"Vector(x = {self.x}, y = {self.y}, lenght={self.lenght})"
@@ -27,9 +18,6 @@ class Vector2:
     
     def __neg__(self):
         return Vector2((self.x * -1, self.y * -1), self.lenght)
-
-    def __truediv__(self, nbr):
-        return Vector2((self.x // nbr, self.y // nbr), self.lenght)
 
     @property
     def indice(self):
